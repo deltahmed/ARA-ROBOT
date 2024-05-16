11d9159a5c5d40eaa5f35d81c55a5df4e4f5df18
RayyyZen <rayanmihani@gmail.com>
2024-05-16 18:33:42 +0200

    upgrade menu

--- src/menu.c
+++ src/menu.c
@@ -1,5 +1,21 @@
 #include "menu.h"
 
+printTitle(ARA_Window self){
+    mvwprintw(self.main_window,1,1,".______        ______     _______  __    __   _______");
+    mvwprintw(self.main_window,2,1,"|   _  \\      /  __  \\   /  _____||  |  |  | |   ____|");
+    mvwprintw(self.main_window,3,1,"|  |_)  |    |  |  |  | |  |  __  |  |  |  | |  |__");
+    mvwprintw(self.main_window,4,1,"|      /     |  |  |  | |  | |_ | |  |  |  | |   __|");
+    mvwprintw(self.main_window,5,1,"|  |\\  \\----.|  `--'  | |  |__| | |  `--'  | |  |____");
+    mvwprintw(self.main_window,6,1,"| _| `._____| \\______/   \\______|  \\______/  |_______|");
+    
+    mvwprintw(self.main_window,7,1," __       __   __  ___  _______");
+    mvwprintw(self.main_window,8,1,"|  |     |  | |  |/  / |   ____|");
+    mvwprintw(self.main_window,9,1,"|  |     |  | |  '  /  |  |__");
+    mvwprintw(self.main_window,10,1,"|  |     |  | |    <   |   __|");
+    mvwprintw(self.main_window,11,1,"|  `----.|  | |  .  \\  |  |____");
+    mvwprintw(self.main_window,12,1,"|_______||__| |__|\\__\\ |_______|");
+}
+
 void printmenu(ARA_Window self, char choice[SIZE_MENU][20], int k){
     //Y a moyen de beaucoup optimiser mais faudra mettre 🔳 et 🔲 dans une variable et utiliser la variable dans les conditions
     int i=0,letters=0;
@@ -10,14 +26,14 @@ void printmenu(ARA_Window self, char choice[SIZE_MENU][20], int k){
             mvwprintw(self.main_window, 4*i+NB_LINES/2-(SIZE_MENU-1)*2, NB_COLS/2-15, "💨");
             mvwprintw(self.main_window, 4*i+NB_LINES/2-(SIZE_MENU-1)*2, NB_COLS/2-10, "🔲");
             mvwprintw(self.main_window, 4*i+NB_LINES/2-(SIZE_MENU-1)*2, 10+NB_COLS/2-10-letters/2, "%s",choice[i]);
-            mvwprintw(self.main_window, 4*i+NB_LINES/2-(SIZE_MENU-1)*2, 20+NB_COLS/2-10, "🔲");
+            mvwprintw(self.main_window, 4*i+NB_LINES/2-(SIZE_MENU-1)*2, 20+NB_COLS/2-9, "🔲");
             //Pour le deuxieme affichage je fais la colonne du premier + du deuxieme sur 2 comme ca je suis au milieux des 2 🔲 puis je recule de lettres/2 
             //Le 4*i c est parce que il y a 4 espaces entre chaque deux chaines
         }
         else{
             mvwprintw(self.main_window, 4*i+NB_LINES/2-(SIZE_MENU-1)*2, NB_COLS/2-10, "🔳");
             mvwprintw(self.main_window, 4*i+NB_LINES/2-(SIZE_MENU-1)*2, 10+NB_COLS/2-10-letters/2, "%s",choice[i]);
-            mvwprintw(self.main_window, 4*i+NB_LINES/2-(SIZE_MENU-1)*2, 20+NB_COLS/2-10, "🔳");
+            mvwprintw(self.main_window, 4*i+NB_LINES/2-(SIZE_MENU-1)*2, 20+NB_COLS/2-9, "🔳");
             //Le -10 il vient de la difference d emplacement entre les deux 🔳 sur deux : (20-0)/2
         }
     }
@@ -49,6 +65,7 @@ int menuGame(Game *game){
     do{
         game->window.clear_all(&game->window);
         game->window.create_one_win_mode(&game->window);
+        //printTitle(game->window);
         printmenu(game->window,choice,k);
         game->window.refresh_all(&game->window);
         game->window.update_key(&game->window);
@@ -58,10 +75,13 @@ int menuGame(Game *game){
 
 int menuChoice(Game *game){
     switch (menuGame(game)){
+        case 0 :
+            generatemap(game);
+            game->timer.reset(&game->timer);
+            return 1;
         case 1 :
             recoverGame(game);
             return 1;
-            break;
         case 4 :
             char choice[SIZE_MENU][20]={"Groupe","MI-3 tri-K","Membres","Ahmed","Abdelwaheb","Rayane"};
             game->window.clear_all(&game->window);
@@ -69,13 +89,9 @@ int menuChoice(Game *game){
             printmenu(game->window, choice, 7);
             game->window.update_key(&game->window);
             game->window.get_key(&game->window);
-            return 0;
-        case 5 :
-            return 0;
-        default :
-            generatemap(game);
-            game->timer.reset(&game->timer);
-            return 1;
+            menuChoice(game);
             break;
+        default :
+            return 0;
     }
 }
