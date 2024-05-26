ba22f49e8d56f96af80acdd791d9391a7984bcd8
deltahmed <fantasi.jump@gmail.com>
2024-05-26 18:41:38 +0200

    update gen

--- src/generator.c
+++ src/generator.c
@@ -549,7 +549,7 @@ void fill_zone_and_doors(Game* game, int x1, int y1, int x2, int y2, Map_def ban
     //game->map.set(&game->map, randint(x1+1,x2-1), randint(y1+1,y2-1), MAP_MONSTER2);
     //game->map.set(&game->map, randint(x1+1,x2-1), randint(y1+1,y2-1), MAP_HEATH_CHARGE); 
     //game->nb_tasks++;
-    nb = randint (1, 4);
+    nb = randint (0, 3);
     for (int i = 0; i < nb; i++)
     {
         rand_x = randint(x1+1,x2-1);
@@ -560,7 +560,7 @@ void fill_zone_and_doors(Game* game, int x1, int y1, int x2, int y2, Map_def ban
             game->nb_tasks++;
         }
     }
-    nb = randint (1, 4);
+    nb = randint (0, 3);
     for (int i = 0; i < nb; i++)
     {
         rand_x = randint(x1+1,x2-1);
@@ -570,7 +570,7 @@ void fill_zone_and_doors(Game* game, int x1, int y1, int x2, int y2, Map_def ban
             game->map.set(&game->map, rand_x, rand_y, randint(MAP_MONSTER, MAP_MONSTER4 +1)); 
         }
     }
-    nb = randint (1, 6);
+    nb = randint (0, 3);
     for (int i = 0; i < nb; i++)
     {
         rand_x = randint(x1+1,x2-1);
